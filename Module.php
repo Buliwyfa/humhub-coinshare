c979cecef7d584085cd4ac43427eb6a52a802eb7
mrkurosawa <KurosawaisMan@gmail.com>
2017-06-15 18:41:56 +0300

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace humhub\modules\coinshare;
+
+use Yii;
+use humhub\models\Setting;
+use yii\helpers\Url;
+
+class Module extends \humhub\components\Module
+{
+    public function getConfigUrl()
+    {
+    }
+
+    public function enable()
+    {
+        parent::enable();
+    }
+
+}
+
+?>
